457c2b1ef293817b7c6e9451c0fe63e33eec5c81
dimas lópez <me@dimaslz.io>
2021-03-29 12:22:27 +0200

    fix(utils): fix type lines

--- utils/read-file.utils.ts
+++ utils/read-file.utils.ts
@@ -1,14 +1,23 @@
 export default function readFile(file: File, cb?: (content: string) => void): Promise<string> {
-	return new Promise(resolve => {
-		let reader = new FileReader();
-		reader.onload = (event: ProgressEvent) => {
-			let result: string | null = (event.target as FileReader).result as string;
-			const match = result.match(/<svg[^]+>[^>]+<\/svg>/gm);
-			const [svg = '']: any[] = match || [];
-			result = svg as string;
-			if (cb) cb(result);
-			resolve(result);
-		};
-		reader.readAsText(file);
+	return new Promise((resolve, reject) => {
+		try {
+			let reader = new FileReader();
+			reader.onload = (event: ProgressEvent) => {
+				if (event.target instanceof FileReader) {
+					const target: FileReader = event.target;
+					if (typeof target.result === 'string') {
+						let result: string = target.result;
+						if (!result) throw new Error('No file content');
+
+						if (cb) cb(result);
+						resolve(result);
+					}
+				}
+			};
+			reader.readAsText(file);
+		}
+		catch (err) {
+			return reject();
+		}
 	});
 }
