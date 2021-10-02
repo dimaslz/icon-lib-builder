914aefc34d7c34f6ed0352f8b7a970f96cf23093
dimaslz <me@dimaslz.io>
2021-10-03 01:34:16 +0200

    fix(env): fix next setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  devIndicators: {
-    autoPrerender: false,
-  },
-  env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-}
+	devIndicators: {
+		autoPrerender: false
+	},
+	env: {
+		API_URL: process.env.NEXT_PUBLIC_API_URL
+	}
+};
