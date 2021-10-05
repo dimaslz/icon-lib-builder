a7f8ef6cf92c14ed3e7b0a3c8e67f8ffdded8164
dimaslz <me@dimaslz.io>
2021-10-05 23:17:28 +0200

    chore(analitycs): add plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,24 @@ import 'tailwindcss/tailwind.css';
 import '../assets/css/style.scss';
 
 import type { AppProps } from 'next/app';
+import { useLayoutEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const { NODE_ENV } = process.env;
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+	useLayoutEffect(() => {
+		if (NODE_ENV === 'production') {
+			const node: HTMLScriptElement = document.createElement('script');
+			node.src = 'https://plausible.io/js/plausible.js';
+			node.type = 'text/javascript';
+			node.async = true;
+			node.defer = true;
+			node.dataset.domain = 'icon-lib-builder.dimaslz.dev';
+
+			document.getElementsByTagName('head')[0].appendChild(node);
+		}
+	});
+
 	return <Component {...pageProps} />;
-}
+};
 export default MyApp;
