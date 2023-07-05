ee67cb1524f98fe9428019e5e71a1f82fc717441
dimaslz <me@dimaslz.io>
2023-07-05 18:19:11 +0200

    test(pages): test loading state

---
+++ pages/index.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+
+import Index from './index';
+
+vi.mock('@/layouts/SVGEditor.layout', () => ({
+	default: () => <div>svg-editor</div>,
+}));
+
+vi.mock('@/layouts/ComponentEditorView.layout', () => ({
+	default: () => <div>component-editor-view</div>,
+}));
+
+vi.mock('react', async () => {
+	const actual: typeof React = await vi.importActual('react');
+
+	return {
+		...actual,
+	};
+});
+
+describe('Index', () => {
+	afterEach(() => {
+		vi.resetAllMocks();
+		vi.resetModules();
+	});
+
+	test('Loading should be present', async () => {
+		const { container } = render(<Index />);
+
+		const style = document.createElement('style');
+		style.innerHTML = '.hidden { visibility: hidden; }';
+		document.body.appendChild(style);
+		document.body.appendChild(container);
+
+		const loadingElm = await screen.queryByTestId('loading');
+		expect(loadingElm).toBeInTheDocument();
+
+		expect(await screen.queryByText('svg-editor')).not.toBeVisible();
+		expect(await screen.queryByText('component-editor-view')).not.toBeVisible();
+	});
+
+	test('Loading should not be present', async () => {
+		vi.spyOn(React, 'useState')
+			.mockImplementationOnce(() => [true, () => null])
+			.mockImplementationOnce(() => [true, () => null]);
+
+		render(<Index />);
+
+		const loadingElm = await screen.queryByTestId('loading');
+		expect(loadingElm).not.toBeInTheDocument();
+
+		expect(await screen.queryByText('svg-editor')).toBeVisible();
+		expect(await screen.queryByText('component-editor-view')).toBeVisible();
+	});
+});
